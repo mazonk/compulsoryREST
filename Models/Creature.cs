4755eae76cdcefd179ab6915a61191a0800e8596
Katarína Brezániová <katkabrezani@gmail.com>
2025-03-03 12:57:00 +0100

    created model class + implementing mongosettings

---
+++ Models/Creature.cs
@@ -0,0 +1,39 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace CompulsoryREST.Models;
+ 
+class Creature {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]    
+    public string? Name { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Species { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Location { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int? Lifespan { get; set; } // in years
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Size { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Status { get; set; }
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Description { get; set; }
+}
